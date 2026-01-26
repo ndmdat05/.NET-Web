4d4ae3e01057e6d407f025cdb868aaee402819b8
QUANG MINH <23130189@st.hcmuaf.edu.vn>
2026-01-26 17:12:40 +0700

    làm đủ thứ

--- WebApplication1/Models/ForgotPasswordViewModel.cs
+++ WebApplication1/Models/ForgotPasswordViewModel.cs
@@ -1,7 +1,11 @@
-﻿namespace WebShop.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebShop.Models
 {
     public class ForgotPasswordViewModel
     {
+        [Required(ErrorMessage = "Email không được để trống")]
+        [EmailAddress(ErrorMessage = "Email không hợp lệ")]
         public string Email { get; set; }
     }
 }
